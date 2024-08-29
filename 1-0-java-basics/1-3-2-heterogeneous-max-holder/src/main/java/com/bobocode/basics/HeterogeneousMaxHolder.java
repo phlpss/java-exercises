6479b7603d090c2910194058312f20a5105177b8
Katya Philip <philipkatya13@gmail.com>
2024-08-29 16:37:06 +0200

    HeterogeneousMaxHolder completed

--- 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
+++ 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
@@ -1,6 +1,10 @@
 package com.bobocode.basics;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link HeterogeneousMaxHolder} is a multi-type container that holds maximum values per each type. It's kind of a
@@ -16,42 +20,63 @@ import java.util.Map;
  */
 public class HeterogeneousMaxHolder {
 
-    /**
-     * A method put stores a provided value by its type, if the value is greater than the current maximum. In other words, the logic
-     * of this method makes sure that only max value is stored and everything else is ignored.
-     * <p>
-     * If the current max value is less than a provided one, or if it's null, then a provided value gets stored and the old
-     * max is returned. Otherwise, nothing new is added, and the provided value is returned.
-     * <p>
-     * So technically, this method always stores the greater value and returns the smaller one.
-     *
-     * @param key   a provided value type
-     * @param value a value to put
-     * @param <T>   value type parameter
-     * @return a smaller value among the provided value and the current maximum
-     */
-    // todo: implement a method according to javadoc
+    private Map<Class<?>, Object> typeToMaxValueMap = new HashMap<>();
 
-    /**
-     * An overloaded method put implements the same logic using a custom comparator. A given comparator is wrapped with
-     * a null-safe comparator, considering null smaller than any non-null object.
-     * <p>
-     * All arguments must not be null.
-     *
-     * @param key        a provided value type
-     * @param value      a value to put
-     * @param comparator a custom comparator for the given type
-     * @param <T>        value type parameter
-     * @return a smaller value among the provided value and the current maximum
-     */
-    // todo: implement a method according to javadoc
+/**
+ * A method put stores a provided value by its type, if the value is greater than the current maximum.
+ * In other words, the logic of this method makes sure that only max value is stored and everything else is ignored.
+ * <p>
+ * If the current max value is less than a provided one, or if it's null, then a provided value gets stored and the old
+ * max is returned. Otherwise, nothing new is added, and the provided value is returned.
+ * <p>
+ * So technically, this method always stores the greater value and returns the smaller one.
+ *
+ * @param key   a provided value type
+ * @param value a value to put
+ * @param <T>   value type parameter
+ * @return a smaller value among the provided value and the current maximum
+ */
 
-    /**
-     * A method getMax returns a max value by the given type. If no value is stored by this type, then it returns null.
-     *
-     * @param key a provided value type
-     * @param <T> value type parameter
-     * @return current max value or null
-     */
-    // todo: implement a method according to javadoc
+    public <T extends Comparable<? super T>> T put(Class<T> key, T value) {
+        return put(key, value, Comparator.naturalOrder());
+    }
+
+/**
+ * An overloaded method put implements the same logic using a custom comparator. A given comparator is wrapped with
+ * a null-safe comparator, considering null smaller than any non-null object.
+ * <p>
+ * All arguments must not be null.
+ *
+ * @param key        a provided value type
+ * @param value      a value to put
+ * @param comparator a custom comparator for the given type
+ * @param <T>        value type parameter
+ * @return a smaller value among the provided value and the current maximum
+ */
+
+    public <T> T put(Class<T> key, T value, Comparator<? super T> comparator) {
+        var currentMax = getMax(requireNonNull(key));
+        var nullSafeComparator = Comparator.nullsFirst(requireNonNull(comparator));
+
+        requireNonNull(value);
+        if (nullSafeComparator.compare(value, currentMax) > 0) {
+            typeToMaxValueMap.put(key, value);
+            return currentMax;
+        }
+        return value;
+    }
+
+/**
+ * A method getMax returns a max value by the given type. If no value is stored by this type, then it returns null.
+ *
+ * @param key a provided value type
+ * @param <T> value type parameter
+ * @return current max value or null
+ */
+
+    public <T> T getMax(Class<T> key) {
+        requireNonNull(key);
+        var currentMax = typeToMaxValueMap.get(key);
+        return key.cast(currentMax);
+    }
 }
