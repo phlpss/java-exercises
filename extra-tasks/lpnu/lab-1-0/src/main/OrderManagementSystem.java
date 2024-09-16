43d5b9d9fd67a8e2d69ec226766333d8da02ee09
Katya Philip <philipkatya13@gmail.com>
2024-09-16 12:58:05 +0300

    remove temporary main function

--- extra-tasks/lpnu/lab-1-0/src/main/OrderManagementSystem.java
+++ extra-tasks/lpnu/lab-1-0/src/main/OrderManagementSystem.java
@@ -4,62 +4,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class OrderManagementSystem {
-    public static void main(String[] args) {
-
-        List<Order> orders = Arrays.asList(
-                new Order("001", "John Doe", Arrays.asList(
-                        new Product("Laptop", 1, 1000.0),
-                        new Product("Mouse", 2, 25.0)), "card"),
-
-                new Order("002", "John Doe", List.of(
-                        new Product("Laptop", 2, 950.0)), "cash"),
-
-                new Order("003", "Alice Boehm", Arrays.asList(
-                        new Product("Keyboard", 1, 45.0),
-                        new Product("Mouse", 2, 20.0),
-                        new Product("Keyboard", 2, 45.0)), "cash"),
-
-                new Order("004", "Victor Boehm", List.of(
-                        new Product("SSD", 3, 70.0),
-                        new Product("Keyboard", 1, 45.0)), "cash"),
-
-                new Order("005", "Alice Boehm", List.of(
-                        new Product("SSD", 3, 70.0),
-                        new Product("Monitor", 2, 200.0),
-                        new Product("Keyboard", 1, 45.0)), "cash"),
-
-                new Order("006", "John Doe", Arrays.asList(
-                        new Product("Laptop", 1, 1000.0),
-                        new Product("Headphones", 1, 150.0)), "card")
-        );
-
-        List<Order> cardOrders = ordersByPaymentMethod(orders, "card");
-        List<Order> cashOrders = ordersByPaymentMethod(orders, "cash");
-        System.out.println("Orders paid by card: " + cardOrders);
-        System.out.println("Orders paid by cash: " + cashOrders);
-
-        Map<String, List<Order>> ordersByCustomer = ordersGroupedByCustomer(orders);
-        System.out.println("Orders grouped by customer: " + ordersByCustomer);
-
-        List<Order> aliceOrders = ordersByCustomerName(orders, "Alice Boehm");
-        System.out.println("Orders by customer name: " + aliceOrders);
-
-        Map<String, Integer> totalProductAmount = totalAmountOfEachProduct(orders);
-        System.out.println("Total amount of each product: " + totalProductAmount);
-
-        List<Order> sortedOrders = sortOrdersByTotalProductsValue(orders);
-        System.out.println("Orders sorted by total value: " + sortedOrders);
-
-        List<Product> uniqueProducts = getUniqueProducts(orders).stream().toList();
-        System.out.println("Unique products: " + uniqueProducts);
-
-        Optional<Order> maxValueOrder = getMaxValueOrder(orders);
-
-        maxValueOrder.ifPresentOrElse(
-                order -> System.out.println("main.Order with highest total value: " + order),
-                () -> System.out.println("No orders available")
-        );
-    }
 
     public static Set<Product> getUniqueProducts(List<Order> orders) {
         return new HashSet<>(orders.stream()
